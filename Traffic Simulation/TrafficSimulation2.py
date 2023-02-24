b75ae231d58da6348d36f10d0cc581752d812cd4
Calvin Brooks <123132758+050cal@users.noreply.github.com>
2023-02-23 21:57:08 -0800

    Update TrafficSimulation2.py
    Recent changes made it so that it no longer recognized bad input. This was due to a workaround for when it tried to find a 'type' attribute when vehicles did not have them. Updated it so that it will only find the type if it exists. This can be adjusted down the road if/when type is required. This same kind of approach can be used later if we decide to import optional attributes which may be helpful for calculations.
    
    I also turned the errors into a list, they now print from the SimpleOutput.py which looks much cleaner. It may also come in useful later down the line so we can print it when and where we want in the GUI.

--- Traffic Simulation/TrafficSimulation2.py
+++ Traffic Simulation/TrafficSimulation2.py
@@ -80,7 +80,8 @@ class TrafficSystem:
         self.roadList = []
         self.trafficLightList = []
         self.vehicleList = []
-        self.vehicleGeneratorList = []          
+        self.vehicleGeneratorList = []
+        self.errorList = []          
 
     def ReadElementsFromFile(self, fileName):
         tree = ET.parse(fileName)
@@ -91,37 +92,40 @@ class TrafficSystem:
                 #error checking for invalid attributes of element - this is done for each element type
                 for subelem in elem:
                     if subelem.tag != "name" and subelem.tag != "length":
-                        print("Bad Input Found: \"",subelem.tag,"\" is not an acceptable attribute of \"",elem.tag,"\"")
+                        self.errorList.append("- \"" + subelem.tag + "\" is not an acceptable attribute of \"" + elem.tag + "\"")
                 name = elem.find("name").text
                 length = int(elem.find("length").text)
                 self.roadList.append({"name": name, "length": length})
             elif elem.tag == "TRAFFICLIGHT":
                 for subelem in elem:
-                    if subelem.tag != "road" and subelem.tag != "position" and subelem.tag != "cycle" and (subelem.tag == "type" and not subelem.text):
-                        print("Bad Input Found: \"",subelem.tag,"\" is not an acceptable attribute of \"",elem.tag,"\"")
+                    if subelem.tag != "road" and subelem.tag != "position" and subelem.tag != "cycle":
+                        self.errorList.append("- \"" + subelem.tag + "\" is not an acceptable attribute of \"" + elem.tag + "\"")
                 road = elem.find("road").text
                 position = int(elem.find("position").text)
                 cycle = int(elem.find("cycle").text)
-                type = elem.find("type").text if elem.find("type") is not None else None
-                self.trafficLightList.append({"road": road, "position": position, "cycle": cycle, "type": type})
+                self.trafficLightList.append({"road": road, "position": position, "cycle": cycle})
             elif elem.tag == "VEHICLE":
+                type = None #if a type is recognized it will be updated - these can be removed down the road when/if type becomes required
                 for subelem in elem:
-                    if subelem.tag != "road" and subelem.tag != "position" and subelem.tag != "speed" and subelem.tag != "acceleration" and (subelem.tag == "type" and not subelem.text):
-                        print("Bad Input Found: \"",subelem.tag,"\" is not an acceptable attribute of \"",elem.tag,"\"")
+                    if subelem.tag != "road" and subelem.tag != "position" and subelem.tag != "speed" and subelem.tag != "acceleration" and subelem.tag != "type":
+                        self.errorList.append("- \"" + subelem.tag + "\" is not an acceptable attribute of \"" + elem.tag + "\"")
+                    if subelem.tag == "type":
+                        type = elem.find("type").text   #recognized a type in the vehicle element so found it - finding when its not there throws an error
                 road = elem.find("road").text
                 position = int(elem.find("position").text)
                 speed = int(elem.find("speed").text)
                 acceleration = int(elem.find("acceleration").text)
-                type = elem.find("type").text if elem.find("type") is not None else None
                 self.vehicleList.append({"road": road, "position": position, "speed": speed, "acceleration": acceleration, "type": type})
             elif elem.tag == "VEHICLEGENERATOR":
+                type = None 
                 for subelem in elem:
-                    if subelem.tag != "name" and subelem.tag != "frequency" and (subelem.tag == "type" and not subelem.text):
-                        print("Bad Input Found: \"",subelem.tag,"\" is not an acceptable attribute of \"",elem.tag,"\"")
+                    if subelem.tag != "name" and subelem.tag != "frequency" and subelem.tag != "type":
+                        self.errorList.append("- \"" + subelem.tag + "\" is not an acceptable attribute of \"" + elem.tag + "\"")
+                    if subelem.tag == "type":
+                        type = elem.find("type").text
                 name = elem.find("name").text
                 frequency = int(elem.find("frequency").text)
-                type = elem.find("type").text if elem.find("type") is not None else None
                 self.vehicleGeneratorList.append({"name": name, "frequency": frequency, "type": type})
             #checking for unacceptable element input
             else:
-                print("Bad Input Found: \"",elem.tag, "\" is not an acceptable element")
+                self.errorList.append("- \"" + elem.tag + "\" is not an acceptable element")
