c1096f72c5a589c52e24162389e656a54843d8b7
ks-moss <k.sinamkam.usa@gmail.com>
2023-03-11 17:43:24 -0800

    Provide comments

--- Traffic Simulation/SimulationIntersection.py
+++ Traffic Simulation/SimulationIntersection.py
@@ -1,6 +1,34 @@
-#3.7
+#3.7 Simulation of intersections
 from AutomaticSimulation import *
 import random
+
+# Goal:
+# A road can contain intersections. Vehicles must choose which road to follow at an intersection.
+# You also need to implement use case 1.5 for this.
+
+# Precondition:
+# The system contains a sceme of the virtual traffic network. This scheme contains at least 2 roads and 1 intersection.
+# Postcondition:Vehicles choose a road at each intersection.
+
+# Notes:
+# If the position of an intersection on a road is equal to the length of the road, then the road ends at that intersection. 
+# Vehicles will therefore not be able to choose this road at this intersection. 
+# If the position of an intersection on a road is zero, then the road starts at that intersection.
+
+# Atributes:
+#   trafficSystem
+#       - AutomaticSimulation
+#   vehicle_state
+#       - vehicle list (Vehicles choose a road at each intersection) 
+#   trafficlight_state
+#       - Store state of all intersections
+# Methods:
+#   is_approaching_N_selected_road
+#       - First check if the vehicle is on thesame road as the intersection is located
+#       - Secondly, check if the current position of hte vehicle is approaching the intersection
+#           - If yes, then make a choice the swap the road
+#           - If no, keep searching
+
 
 APPROACHING_DISTANCE = 5
 
