7fdb42e3387429dd4d138fc565a8e335349a2889
Dashtine <layson@unlv.nevada.edu>
2023-04-30 16:52:09 -0700

    Fixed Unit Test File for TrafficLightSimulation
    This update fixes all bugs from the previous version of the unit test and is coordinated with the project's current state.
    All 6 functions of the unit are tested and successful.

--- Traffic Simulation/TrafficLightSimulationUnitTests.py
+++ Traffic Simulation/TrafficLightSimulationUnitTests.py
@@ -14,81 +14,80 @@ import TrafficLightSimulation
 #   6. When light is switched to red, all vehicles in front of the traffic
 
 
+
 class TestTrafficLightSimulation(unittest.TestCase):
 
     # Test that green light changes to red after cycle time
-    def test_green_light_changes_to_red(self):
-        trafficLight = {"road": "Main St", "position": 10, "cycle": 10}
+    def test_green_light_changes_to_yellow(self):
+        trafficLight = [{"road": "Main St", "position": 10, "cycle": 10}]
+        cycleTimeCalc = trafficLight[0]["cycle"]/1.5
+        lightStates = [{"color": "green", "counter": cycleTimeCalc}]
         vehicles = []
-        timeSinceLastChange = 11
-        TrafficLightSimulation.green_light = True
-        expected_light = False
-        TrafficLightSimulation.trafficLightInteraction(trafficLight, vehicles, timeSinceLastChange)
-        error_msg = "Error: Green light did not change to red!"
-        self.assertEqual(expected_light, TrafficLightSimulation.green_light, error_msg)
+        expected_light = "yellow"
+        TrafficLightSimulation.trafficLightInteraction(trafficLight, vehicles, 0, lightStates)
+        error_msg = "Error: Green light did not change to yellow!"
+        self.assertEqual(expected_light, lightStates[0]["color"], error_msg)
     
     # Test that green light stays green if cycle time is less than timeSinceLastChange
     def test_green_light_stays_green(self):
-        trafficLight = {"road": "Main St", "position": 10, "cycle": 10}
+        trafficLight = [{"road": "Main St", "position": 10, "cycle": 10}]
+        cycleTimeCalc = (trafficLight[0]["cycle"]/1.5) - 1
+        lightStates = [{"color": "green", "counter": cycleTimeCalc}]
         vehicles = []
-        timeSinceLastChange = 5
-        TrafficLightSimulation.green_light = True
-        expected_light = True
-        TrafficLightSimulation.trafficLightInteraction(trafficLight, vehicles, timeSinceLastChange)
-        error_msg = "Error: Green light did not stay green!"
-        self.assertEqual(expected_light, TrafficLightSimulation.green_light, error_msg)
+        expected_light = "green"
+        TrafficLightSimulation.trafficLightInteraction(trafficLight, vehicles, 0, lightStates)
+        error_msg = "Error: Green light did not stay on green!"
+        self.assertEqual(expected_light, lightStates[0]["color"], error_msg)
 
 
     # Test that red light changes to green after cycle time
     def test_red_light_changes_to_green(self):
-        trafficLight = {"road": "Main St", "position": 10, "cycle": 10}
+        trafficLight = [{"road": "Main St", "position": 10, "cycle": 10}]
+        cycleTimeCalc = trafficLight[0]["cycle"]+1
+        lightStates = [{"color": "red", "counter": cycleTimeCalc}]
         vehicles = []
-        timeSinceLastChange = 11
-        TrafficLightSimulation.green_light = False
-        expected_light = True
-        TrafficLightSimulation.trafficLightInteraction(trafficLight, vehicles, timeSinceLastChange)
+        expected_light = "green"
+        TrafficLightSimulation.trafficLightInteraction(trafficLight, vehicles, 0, lightStates)
         error_msg = "Error: Red light did not change to green!"
-        self.assertEqual(expected_light, TrafficLightSimulation.green_light, error_msg)
+        self.assertEqual(expected_light, lightStates[0]["color"], error_msg)
 
     # Test that red light stays red if cycle time is less than timeSinceLastChange
     def test_red_light_stays_red(self):
-        trafficLight = {"road": "Main St", "position": 10, "cycle": 10}
+        trafficLight = [{"road": "Main St", "position": 10, "cycle": 10}]
+        cycleTimeCalc = trafficLight[0]["cycle"]-1
+        lightStates = [{"color": "red", "counter": cycleTimeCalc}]
         vehicles = []
-        timeSinceLastChange = 5
-        TrafficLightSimulation.green_light = False
-        expected_light = False
-        TrafficLightSimulation.trafficLightInteraction(trafficLight, vehicles, timeSinceLastChange)
-        error_msg = "Error: Red light did not stay red!"
-        self.assertEqual(expected_light, TrafficLightSimulation.green_light, error_msg)
+        expected_light = "red"
+        TrafficLightSimulation.trafficLightInteraction(trafficLight, vehicles, 0, lightStates)
+        error_msg = "Error: Red light did not change to green!"
+        self.assertEqual(expected_light, lightStates[0]["color"], error_msg)
 
 
     # Test that acceleration is invoked for vehicles in front of the traffic light when it is green
     def test_acceleration_is_invoked_when_green(self):
-        trafficLight = {"road": "Main St", "position": 10, "cycle": 10}
-        vehicles = [{"road": "Main St", "position": 8, "acceleration": 0},
-                    {"road": "Main St", "position": 4, "acceleration": 0}]
-        timeSinceLastChange = 5
-        TrafficLightSimulation.green_light = True
-        expected_vehicles = [{"road": "Main St", "position": 8, "acceleration": VehicleCalculations.maxAcceleration},
-                             {"road": "Main St", "position": 4, "acceleration": VehicleCalculations.maxAcceleration}]
-        actual_vehicles = TrafficLightSimulation.trafficLightInteraction(trafficLight, vehicles, timeSinceLastChange)
+        trafficLight = [{"road": "Main St", "position": 10, "cycle": 10}]
+        vehicles = [{"road": "Main St", "position": 8, "speed": 0,"acceleration": 0},
+                    {"road": "Main St", "position": 4, "speed": 0,"acceleration": 0}]
+        cycleTimeCalc = trafficLight[0]["cycle"]+1
+        lightStates = [{"color": "red", "counter": cycleTimeCalc}]
+        expected_vehicles = [{"road": "Main St", "position": 8, "speed": 8, "acceleration": 2},
+                             {"road": "Main St", "position": 4, "speed": 8, "acceleration": 2}]
+        TrafficLightSimulation.trafficLightInteraction(trafficLight, vehicles, 0, lightStates)
         error_msg = "Error: Acceleration was not adjusted for vehicles in front of the green light!"
-        self.assertEqual(expected_vehicles, actual_vehicles, error_msg)
+        self.assertEqual(expected_vehicles, vehicles, error_msg)
 
     # Test that vehicles within deceleration distance slow down when light turns red
     def test_vehicles_within_deceleration_distance_slow_down_when_red(self):
-        trafficLight = {"road": "Main St", "position": 10, "cycle": 10}
-        vehicles = [{"road": "Main St", "position": 10, "acceleration": 0},
-                    {"road": "Main St", "position": 7, "acceleration": 0},
-                    {"road": "Main St", "position": 3, "acceleration": 0}]
-        timeSinceLastChange = 11
-        TrafficLightSimulation.green_light = True
-        expected_vehicles = [{"road": "Main St", "position": 10, "acceleration": VehicleCalculations.maxAcceleration},
-                             {"road": "Main St", "position": 7, "acceleration": VehicleCalculations.maxAcceleration/2},
-                             {"road": "Main St", "position": 3, "acceleration": 0}]
-        actual_vehicles = TrafficLightSimulation.trafficLightInteraction(trafficLight, vehicles, timeSinceLastChange)
+        trafficLight = [{"road": "Main St", "position": 10, "cycle": 10}]
+        vehicles = [{"road": "Main St", "position": 4, "speed": 8, "acceleration": 2, "type": "car"},
+                    {"road": "Main St", "position": 1, "speed": 8, "acceleration": 2, "type": "car"}]
+        cycleTimeCalc = trafficLight[0]["cycle"]+1
+        lightStates = [{"color": "yellow", "counter": cycleTimeCalc}]
+        expected_vehicles = [{"road": "Main St", "position": 4, "speed": 8, "acceleration": -0.4443373493975904, "type": "car"},
+                             {"road": "Main St", "position": 1, "speed": 8, "acceleration": -0.4443373493975904, "type": "car"}]
+        TrafficLightSimulation.trafficLightInteraction(trafficLight, vehicles, 0, lightStates)
         error_msg = "Error: Deceleration calculations was not adjusted!"
-        self.assertEqual(expected_vehicles, actual_vehicles, error_msg)
+        self.assertEqual(expected_vehicles, vehicles, error_msg) 
 
 # Call all functions within this file for a test
 if __name__ == '__main__':
